58f368089743ea1ea7f8028ef138e2efdc2f4d22
Abdelrahman Hany <boody753107@gmail.com>
2023-08-28 03:40:42 +0300

    changes

--- Task 1/Question 1/Q1.v
+++ Task 1/Question 1/Q1.v
@@ -4,7 +4,7 @@ module Task1 (A, B, D, OUT);
 	output OUT;
 	wire S1, S2;
 
-        and(S1, D[0], D[1]);
-        xnor(S2, A, B, D[2]);
-        or(OUT, S1, S2);
+	and(S1, D[0], D[1]);
+	xnor(S2, A, B, D[2]);
+	or(OUT, S1, S2);
 endmodule
